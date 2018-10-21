0f8d9c046f9b78d7b9852eb2401504e49c27dd8b
larshelg <qi2greos>
2018-10-21 19:46:32 +0200

    added calculators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, OnDestroy, Output} from '@angular/core';
+import {FormService} from './form.service';
+
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `<div class="container">
+    <nav>
+      <a routerLink="/norrisk" routerLinkActive="active">Norrisk kalkulator ></a>
+      <a routerLink="/chads" routerLinkActive="active">Chads kalkulator ></a>
+    </nav>
+    <router-outlet></router-outlet>
+    <footer class="footer is-primary">
+      <div class="content has-text-centered">
+        <p>
+          <strong>DEMO</strong> 
+        </p>
+        <div>Responsive demo of two heath calculators. Should work on both desktop and mobile</div>
+      </div>
+    </footer>
+  
+  </div>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,40 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NorriskFormComponent } from './norrisk-form/norrisk-form.component';
+import {FormService} from './form.service';
+import { ChadsFormComponent } from './chads-form/chads-form.component';
+import { NorriskControllerComponent } from './norrisk-controller/norrisk-controller.component';
+import { ChadsControllerComponent } from './chads-controller/chads-controller.component';
+
+
+const appRoutes: Routes = [
+  { path: 'norrisk', component: NorriskControllerComponent },
+  { path: '', component: NorriskControllerComponent },
+  { path: 'chads',    component: ChadsControllerComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NorriskFormComponent,
+    ChadsFormComponent,
+    NorriskControllerComponent,
+    ChadsControllerComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
-  providers: [],
+  providers: [FormService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
